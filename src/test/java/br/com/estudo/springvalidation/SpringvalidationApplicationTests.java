780828e560abbbbee16989c5bca0a81444d24ddd
Jhack.Costa <jhack.oliveira@singlepoint.com.br>
2023-05-09 08:51:07 -0300

    teste para classe principal

--- src/test/java/br/com/estudo/springvalidation/SpringvalidationApplicationTests.java
+++ src/test/java/br/com/estudo/springvalidation/SpringvalidationApplicationTests.java
@@ -1,13 +1,15 @@
 package br.com.estudo.springvalidation;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringvalidationApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void main() {
+		SpringvalidationApplication.main(new String[]{});
 	}
 
 }
